502bde251b5cae4ff967f2199dbae713aef9796b
Seema Shishani <semasherdo@gmail.com>
2026-09-01 02:17:31 +0300

    Add first JavaScript program

---
+++ week-01/hello.js
@@ -0,0 +1,54 @@
+const Name = "seema";
+let Age = 19;
+const University = "Jerash Private University";
+console.log(`My name is ${Name}` + ", I am " + Age + " years old.");
+
+let a = 7;
+let b = 3;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
+
+let price = 10;
+let quantity = 4;
+console.log(price * quantity);
+
+
+console.log(typeof 5);
+console.log(typeof "5");
+console.log(typeof true);
+
+let number = 10;
+if (number % 2 === 0) {
+    console.log("Even");
+} else {
+    console.log("Odd");
+}
+
+
+
+let grade = 80;
+if (grade >= 50) {
+    console.log("Pass");
+} else {
+    console.log("Fail");
+}
+
+let number1 = 12;
+let number2 = 10;
+if (number1 > number2) {
+    console.log(number1);
+} else {
+    console.log(number2);
+}
+
+let temperature = 25;
+if (temperature > 30) {
+    console.log("Hot");
+} else if (temperature >= 15) {
+    console.log("Nice");
+} else {
+    console.log("Cold");
+}
