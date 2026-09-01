df7c1148d6b3f430b5f2e0b2f49ef62d660529c7
Seema Shishani <semasherdo@gmail.com>
2026-09-01 22:29:33 +0300

    Add JavaScript practice exercises

--- training/practice.js
+++ training/practice.js
@@ -2,13 +2,19 @@ for (let i = 1; i <= 20; i += 2) {
     console.log(i);
 }
 
+
+
 for (let i = 2; i <= 30; i += 2) {
     console.log(i);
 }
 
+
+
 for (let i = 20; i>=1; i-=2) {
     console.log(i);
 }
+
+
 
 let sum=0;
 for (let i=0 ; i<=50; i++){
@@ -16,10 +22,14 @@ for (let i=0 ; i<=50; i++){
 }
 console.log(sum);
 
+
+
 let x=5;
 for (let i = 1; i<=10; i++){
     console.log(`${x} x ${i} = ${x*i}`);
 }
+
+
  
 for (let i = 1; i<=20 ; i++){
     if (i%2===0) {
@@ -30,6 +40,8 @@ for (let i = 1; i<=20 ; i++){
     }    
 }
 
+
+
 for(let i =1; i<=50 ; i++){
     if(i%5===0){
         console.log("five");
@@ -39,6 +51,8 @@ for(let i =1; i<=50 ; i++){
     }
 }
 
+
+
 let i = 10;
 
 while (i >= 1) {
@@ -46,11 +60,15 @@ while (i >= 1) {
     i--;
 }
 
+
+
 let total=0;
 for ( let i = 2 ; i<=20 ; i+=2){
     total +=i;
 }
 console.log(total);
+
+
 
 let a=15;
 let b=27;
@@ -59,4 +77,88 @@ if (a>b){
 }
 else {
     console.log(`${b} is greater than ${a}`);
+}
+
+
+
+let name = "Seema";
+let age = 19 ;
+let major = "Computer Science";
+let grade=96;
+console.log(`My name is ${name},I am ${age} years old,and I study ${major}`);
+
+if(grade>=50){
+    console.log("pass");
+}
+
+
+let f=12;
+let g=4;
+
+console.log(f+g);
+console.log(f-g);
+console.log(f*g);
+console.log(f/g);
+console.log(f%g);
+
+
+
+let price = 15;
+let quantity = 6;
+console.log(`The total is ${price * quantity}`);
+
+
+
+console.log(typeof 25);
+console.log(typeof "25");
+console.log(typeof false);
+
+
+
+let number=17;
+if(number%2===0){
+    console.log("even");
+}
+else 
+{ console.log("odd") };
+
+
+
+let n=24;
+if(n%2===0){
+    console.log("even");
+}
+else 
+{ console.log("odd") };
+
+
+
+let mark = 44;
+if(mark >=50){
+    console.log("pass"); }
+else{
+        console.log("fail");
+}
+
+
+
+let p = 35;
+let y = 28;
+if(p>y){
+    console.log(p);
+}
+else{
+    console.log(y);
+}
+
+
+let temperature = 18;
+if(temperature>=30){
+    console.log("Hot");
+}
+else if (temperature>=15){
+    console.log("Nice");
+}
+else {
+    console.log(Cold);
 }
