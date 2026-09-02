fe841c4a81cccaac7a8bcf2d68e9197c92d9dfa2
Seema Shishani <semasherdo@gmail.com>
2026-09-02 14:04:30 +0300

    Add JavaScript functions practice

--- week-01/2026_09_02_functions.js
+++ week-01/2026_09_02_functions.js
@@ -81,7 +81,6 @@ function table(){
         console.log(`${x} x ${i} = ${x * i}`);
     }
 }
-
 table();
 
 
@@ -91,5 +90,25 @@ function evenNumbers(){
         console.log(i);
     }
 }
-
 evenNumbers();
+
+
+
+function isPrime(n) {
+    if (n < 2){
+       return false;
+    }
+
+    for (let i = 2; i < n; i++){
+        if (n % i === 0){
+            return false;
+        }
+    }
+    return true;
+}
+console.log(isPrime(7));
+console.log(isPrime(10));
+console.log(isPrime(2));
+console.log(isPrime(1));
+console.log(isPrime(13));
+
