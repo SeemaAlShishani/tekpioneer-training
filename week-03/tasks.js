885b7e81dc8c7d79a61e67cddb65347f6a1eb341
Seema Shishani <semasherdo@gmail.com>
2026-09-20 12:25:51 +0300

    Complete Week 3 Day 4 tasks

--- week-03/tasks.js
+++ week-03/tasks.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
+
 const command = process.argv[2];
 const text = process.argv[3];
+const taskNumber = Number(process.argv[3]);
 
 function loadTasks() {
   if (!fs.existsSync("tasks.json")) {
@@ -14,13 +16,17 @@ function loadTasks() {
 
 if (command === "add") {
   const tasks = loadTasks();
+
   tasks.push({
     id: tasks.length + 1,
     text: text,
     done: false,
   });
+
   const tasksText = JSON.stringify(tasks, null, 2);
+
   fs.writeFileSync("tasks.json", tasksText);
+
   console.log(`Added: ${text}`);
 } else if (command === "list") {
   const tasks = loadTasks();
@@ -29,6 +35,44 @@ if (command === "add") {
     const mark = task.done ? "[x]" : "[ ]";
     console.log(`${task.id}. ${mark} ${task.text}`);
   });
+
+  const doneCount = tasks.filter((task) => task.done).length;
+
+  console.log(`${tasks.length} tasks · ${doneCount} done`);
+} else if (command === "done") {
+  const tasks = loadTasks();
+  const task = tasks[taskNumber - 1];
+
+  if (!task) {
+    console.log("Task not found. Please enter a valid task number.");
+  } else {
+    task.done = true;
+
+    const tasksText = JSON.stringify(tasks, null, 2);
+
+    fs.writeFileSync("tasks.json", tasksText);
+
+    console.log(`Done: ${task.text}`);
+  }
+} else if (command === "delete") {
+  const tasks = loadTasks();
+  const task = tasks[taskNumber - 1];
+
+  if (!task) {
+    console.log("Task not found. Please enter a valid task number.");
+  } else {
+    tasks.splice(taskNumber - 1, 1);
+
+    tasks.forEach((task, index) => {
+      task.id = index + 1;
+    });
+
+    const tasksText = JSON.stringify(tasks, null, 2);
+
+    fs.writeFileSync("tasks.json", tasksText);
+
+    console.log(`Deleted: ${task.text}`);
+  }
 } else {
-  console.log("Use: add or list");
+  console.log("Use: add, list, done, or delete");
 }
